e97174c1ee3b59ecd95e55f728c48678cc279d40
Leonardo Almeida <leonardoalmeida7777@gmail.com>
2024-04-23 16:22:19 +0100

    .

--- website_dep/nginx/run.sh
+++ website_dep/nginx/run.sh
@@ -3,7 +3,6 @@ sudo docker build -t registry.deti/gic-wesago/nginxwebsite:v1 .
 sudo docker push registry.deti/gic-wesago/nginxwebsite:v1
 
 # configmap
-kubectl delete configmap nginx-website-conf --namespace=gic-wesago
 kubectl create configmap nginx-website-conf --namespace=gic-wesago --from-file=nginx.conf
 # deployment
 kubectl apply -f deployment.yaml
