eb9852e9a2556fbf341cd1eb400234f5b3bdb51d
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2024-06-06 16:44:10 +0100

    Add monitoring and observability using grafana

---
+++ monitoring/grafana/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function apply {
+    kubectl apply -f configmap.yaml
+    kubectl apply -f configmap-dashboards.yaml
+    kubectl apply -f deployment.yaml
+    kubectl apply -f service.yaml
+}
+
+function delete {
+    kubectl delete -f .
+}
+
+if [ "$1" == "apply" ]; then
+    apply
+elif [ "$1" == "delete" ]; then
+    delete
+else
+    echo "Invalid argument. Please provide 'apply' or 'delete'."
+fi
